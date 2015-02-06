5d976f06ded9baaa7b07ffabffe24ef1858208f0
kahtras <sarthag@g.clemson.edu>
2015-02-05 20:09:03 -0600

    ajax 1.2

--- Ajax/js/Ajax_js.js
+++ Ajax/js/Ajax_js.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$('.contacts').hide();
-	//if ($('#select').text() == "Account "){
 	$('.searchbtn').click(function(event) {
 		event.preventDefault();
 		$('.contacts').show();
@@ -9,19 +8,21 @@ $(document).ready(function() {
 			url : 'data/data.json',
 			success : function(contacts) {
 				$.each(contacts, function(key, value) {
+					//if ($('#select').text() == "Account "){
 					if ($('#Search').val() == value.Account) {
 						var tr = "<tr><td>" + (key + 1) + "</td><td>" + value.FirstName + "</td><td>" + value.LastName + "</td><td>" + value.StreetAddress + "</td><td>" + value.City + "</td><td>" + value.State + "</td><td>" + value.ZipCode + "</td><td>" + value.Account + "</td></tr>";
 						$('.contacts tbody').append(tr);
 					}
+					//	}
 				});
 			},
 			error : function() {
 				alert("ERROR!!");
 			}
 		});
-	//	$('#Search').val("");
+		//	$('#Search').val("");
 	});
-//	}
+
 	$('.showAll').click(function(event) {
 		event.preventDefault();
 		$('.contacts').show();
