cf07a27a64b7a7e6f636d944506d0d3c0511468a
kahtras <sarthag@g.clemson.edu>
2015-02-07 21:11:59 -0600

    dashboard v1.3

--- Dashboard/dashboard_js.js
+++ Dashboard/dashboard_js.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-	$('#signin').click(function(){
-	var email = $('#email').val();
-	var password = $('#password').val();
-	$.ajax({
-		url:'Dashboard_users.json',
-		success: function(result){
-			$.each(result, function(key, value){
-				if(email == value.username && password == value.password){
-					console.log("yes");
-				}
-			});
-		}
+$(document).ready(function() {
+	$('#signin').click(function() {
+		var email = $('#email').val();
+		var password = $('#password').val();
+		$.ajax({
+			url : 'Dashboard_users.json',
+			success : function(result) {
+				$.each(result, function(key, value) {
+					if (email == value.username && password == value.password) {
+						console.log("yes");
+					}
+				});
+			}
+		});
 	});
-});
 });
