a614248260a05c8339b559605de4a645c63b3694
kahtras <sarthag@g.clemson.edu>
2015-02-07 20:48:21 -0600

    dashboard

--- Ajax/js/Ajax_js.js
+++ Ajax/js/Ajax_js.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
+	var selected = "";
+	$('.select-opt a').click(function(){
+		console.log("dd")
+		selected = $(this).text()
+		console.log(selected)
+	})
 	$('.contacts').hide();
 	$('.searchbtn').click(function(event) {
 		event.preventDefault();
 		$('.contacts').show();
 		$('.contacts tbody').empty();
+		//console.log($('#options').val())
+		var searchAcc = $('#Search').val();
 		$.ajax({
 			url : 'data/data.json',
 			success : function(contacts) {
 				$.each(contacts, function(key, value) {
-					//if ($('#select').text() == "Account "){
-					if ($('#Search').val() == value.Account) {
+					//if (selected == "Account "){
+						console.log($('#Search').val())
+						console.log(value.Account)
+					if (searchAcc == value.Account) {
 						var tr = "<tr><td>" + (key + 1) + "</td><td>" + value.FirstName + "</td><td>" + value.LastName + "</td><td>" + value.StreetAddress + "</td><td>" + value.City + "</td><td>" + value.State + "</td><td>" + value.ZipCode + "</td><td>" + value.Account + "</td></tr>";
 						$('.contacts tbody').append(tr);
 					}
@@ -20,7 +30,7 @@ $(document).ready(function() {
 				alert("ERROR!!");
 			}
 		});
-		//	$('#Search').val("");
+			$('#Search').val("");
 	});
 
 	$('.showAll').click(function(event) {
---
+++ Dashboard/dashboard_js.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$.ajax({
+		url:"Dashboard_users.json"
+	})
+});
