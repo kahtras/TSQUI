4733c28a8f89be635787a40a191f3612d4e5cd23
kahtras <sarthag@g.clemson.edu>
2015-02-09 17:20:07 -0600

    dashboard v1.4

--- Dashboard/dashboard_js.js
+++ Dashboard/dashboard_js.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	$('#signin').click(function() {
+//	$('Form').validate(event1);
+//	event1.preventDefault();
+	$('#signin').click(function(event) {
+		event.preventDefault();
 		var email = $('#email').val();
 		var password = $('#password').val();
 		$.ajax({
@@ -8,8 +11,18 @@ $(document).ready(function() {
 				$.each(result, function(key, value) {
 					if (email == value.username && password == value.password) {
 						console.log("yes");
+						$('#error').text(" ");
+						window.location = "Dashboard_main.html";
+					} else {
+						console.log("no");
+						$('#error').text("Please enter valid user name or password");
+					
 					}
 				});
+
+			},
+			error : function() {
+				console.log("wrong");
 			}
 		});
 	});
